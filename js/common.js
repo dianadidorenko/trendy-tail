486f585a69226350775dd0b8071cafb8bb517ed2
Diana Didorenko <125818555+dianadidorenko@users.noreply.github.com>
2023-08-26 16:58:48 +0300

    .

--- js/common.js
+++ js/common.js
@@ -128,16 +128,3 @@ $(function () {
   });
 });
 // Aside slider language and theme change menu
-
-// Show Empty Block in Shopping Cart
-if (!localStorage.getItem("orderItems")) {
-  orderBlock.style.display = "none";
-  emptyOrderBlock.style.display = "flex";
-} else if (localStorage.getItem("orderItems").length <= 2) {
-  orderBlock.style.display = "none";
-  emptyOrderBlock.style.display = "flex";
-} else if (localStorage.getItem("orderItems")) {
-  orderBlock.style.display = "flex";
-  emptyOrderBlock.style.display = "none";
-}
-// Show Empty Block in Shopping Cart
