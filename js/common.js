2015f48936b96853f36c115317ba1ec244c6400e
Diana Didorenko <125818555+dianadidorenko@users.noreply.github.com>
2023-09-01 14:57:28 +0300

    anchor bhv change + change adaptive

--- js/common.js
+++ js/common.js
@@ -121,3 +121,19 @@ $(function () {
   });
 });
 // Aside slider language and theme change menu
+
+// Smooth  scrolling behavior
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  if (anchor.id == "top") {
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+      const blockID = anchor.getAttribute("href");
+      document
+        .querySelector("" + blockID)
+        .scrollIntoView({ behavior: "smooth", block: "start" });
+    });
+  }
+}
+// Smooth  scrolling behavior
--- js/items.js
+++ js/items.js
@@ -8,3 +8,19 @@ closeSizesTable.addEventListener("click", () => {
   sizesTable.style.display = "none";
 });
 // Show sizes table
+
+// Smooth  scrolling behavior
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  if (anchor.id == "sizesTable") {
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+      const blockID = anchor.getAttribute("href");
+      document
+        .querySelector("" + blockID)
+        .scrollIntoView({ behavior: "smooth", block: "start" });
+    });
+  }
+}
+// Smooth  scrolling behavior
